1085af4f39f1c43efbfd06cb6d2f88e8e174f64a
miro83kolev <miro83kolev@gmail.com>
2023-03-06 14:57:39 +0200

    update

--- JS_Advanced/Object and classes/meetings.js
+++ JS_Advanced/Object and classes/meetings.js
@@ -1,29 +1,21 @@
 function meetings(meetings) {
-    let scheduledMeetings = {};
-    let conflicts = [];
-  
-    meetings.forEach((meeting) => {
-      let [weekday, person] = meeting.split(' ');
-      if (scheduledMeetings[weekday]) {
-        conflicts.push(weekday);
-        return;
-      }
-      scheduledMeetings[weekday] = person;
-      console.log(`Scheduled for ${weekday}`);
-    });
-  
-    console.log('');
-    Object.keys(scheduledMeetings).forEach((weekday) => {
-      console.log(`${weekday} -> ${scheduledMeetings[weekday]}`);
-    });
-  
-    conflicts.forEach((weekday) => {
-      console.log(`Conflict on ${weekday}!`);
-    });
+  let scheduledMeetings = {};
+  let conficts = [];
+
+  for (let meeting of meetings) {
+    let [day, name] = meeting.split(" ");
+    if (scheduledMeetings[day]) {
+      console.log(`Conflict on ${day}!`);
+      conficts.push(day);
+      continue;
+    }
+    scheduledMeetings[day] = name;
+    console.log(`Scheduled for ${day}`);
   }
 
-meetings(['Monday Peter',
-'Wednesday Bill',
-'Monday Tim',
-'Friday Tim']
-)
+  for (let [day, name] of Object.entries(scheduledMeetings)) {
+    console.log(`${day} -> ${name}`);
+  }
+}
+meetings(["Monday Peter", "Wednesday Bill", "Monday Tim", "Friday Tim"]);
+
---
+++ JS_Advanced/Object and classes/songs.js
@@ -0,0 +1,26 @@
+function songs(input) {
+  class Song {
+    constructor(type, name, time) {
+      this.type = type;
+      this.name = name;
+      this.time = time;
+    }
+  }
+
+  let songs = [];
+  let numberOfSongs = input.shift();
+  let typeSong = input.pop();
+
+  for (let index = 0; index < numberOfSongs; index++) {
+    let [type, name, time] = input[index].split("_");
+    let song = new Song(type, name, time);
+    songs.push(song);
+  }
+
+  if (typeSong === "all") {
+    songs.forEach((i) => console.log(i.name));
+  } else {
+    let filtered = songs.filter((i) => i.type === typeSong);
+    filtered.forEach((i) => console.log(i.name));
+  }
+}

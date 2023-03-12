383cfb6146d99f508e039daae4687b742c65dcba
miro83kolev <miro83kolev@gmail.com>
2023-03-12 11:53:59 +0200

    update

---
+++ JS_Advanced/DOM and events/search in list.js
@@ -0,0 +1,19 @@
+function search() {
+  let inputWord = document.getElementById("searchText").value;
+  let townsElement = Array.from(document.querySelectorAll("#towns li"));
+  let match = document.getElementById("result");
+  let count = 0;
+
+  for (let town of townsElement) {
+    if (town.textContent.includes(inputWord)) {
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+      count++;
+    } else {
+      town.style.fontWeight = "normal";
+      town.style.textDecoration = "none";
+    }
+  }
+
+  match.textContent = `${count} matches found`
+}

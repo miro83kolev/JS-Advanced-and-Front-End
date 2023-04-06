d02b486a48ee121e319ce662a591ccdcf6e556ee
miro83kolev <miro83kolev@gmail.com>
2023-04-06 12:16:37 +0300

    update

---
+++ JS_Advanced/Exam preps/plan discovery.js
@@ -0,0 +1,18 @@
+function planDiscovery(input) {
+    let n = Number(input.shift());
+    plants = {};
+    
+  
+}
+planDiscovery((["3",
+"Arnoldii<->4",
+"Woodii<->7",
+"Welwitschia<->2",
+"Rate: Woodii - 10",
+"Rate: Welwitschia - 7",
+"Rate: Arnoldii - 3",
+"Rate: Woodii - 5",
+"Update: Woodii - 5",
+"Reset: Arnoldii",
+"Exhibition"])
+)

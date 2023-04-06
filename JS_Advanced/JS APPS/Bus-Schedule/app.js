cf609ca467968403d9eefe69db4862c88448db9c
miro83kolev <miro83kolev@gmail.com>
2023-04-06 14:20:34 +0300

    update

--- JS_Advanced/JS APPS/Bus-Schedule/app.js
+++ JS_Advanced/JS APPS/Bus-Schedule/app.js
@@ -1,11 +1,25 @@
 function solve() {
+    const label = document.querySelector('#info span');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn  = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stop = {
+        next: 'depot',
+    }
+
+    async function depart() {
+        departBtn.setAttribute('disabled', true);
+        const BASE_URL = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+        const response = await fetch (BASE_URL);
+        stop = await response.json();
+        label.textContent = `Next stop ${stop.name}`;
+        arriveBtn.removeAttribute('disabled', true);
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        label.textContent = `Arriving at ${stop.name}`;
+        departBtn.removeAttribute('disabled', true);
+        arriveBtn.setAttribute('disabled', true);
     }
 
     return {
--- JS_Advanced/JS APPS/Bus-Stop/app.js
+++ JS_Advanced/JS APPS/Bus-Stop/app.js
@@ -1,3 +1,62 @@
+async function getInfo() {
+    
+    const stopNameElement = document.getElementById('stopName');
+    const timeTableElement = document.getElementById('buses');
+
+    const stopId = document.getElementById('stopId').value;
+    const BASE_URL = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    try {
+        timeTableElement.innerHTML = '';        
+        const res = await fetch (BASE_URL);
+        const data = await res.json();
+
+        if (res.status !== 200) {
+            throw new Error('StopId not found')
+        }
+
+        stopNameElement.textContent = data.name;
+
+        Object.entries(data.buses).forEach(bus => {
+            const liItem = document.createElement('li');
+            liItem.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`
+            timeTableElement.appendChild(liItem);
+        });        
+
+    }catch (error) {
+        stopNameElement.textContent = 'Error';
+    }
+}
+
+//Promises solution
+
 function getInfo() {
-    console.log("TODO...");
+    let submitBtn = document.getElementById('submit');
+    let busList = document.getElementById('buses');
+    let stop = document.getElementById('stopName');
+ 
+    submitBtn.addEventListener('click', () => {
+        let idVal = document.getElementById('stopId').value;
+        let url = `http://localhost:3030/jsonstore/bus/businfo/${idVal}`;
+ 
+ 
+        fetch(url)
+            .then(data => data.json())
+            .catch(err => {stop.textContent = 'Error'})
+            .then(data => {
+                stop.textContent = '';
+                busList.innerHTML = '';
+                stop.textContent = data.name;
+                for (entry in data.buses) {
+                    let listEl= document.createElement('li');
+                    listEl.textContent = `Bus ${entry} arrives in ${data.buses[entry]} minutes`
+                    busList.appendChild(listEl);
+                }
+ 
+            })
+            // Not sure if this catch is needed, but want to make sure that we don't have invalid entry in bus.json
+            .catch(err => {stop.textContent = 'Error'})
+ 
+ 
+    })
 }

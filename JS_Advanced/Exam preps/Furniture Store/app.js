b40003e594cd1e660bedb04f8a154089c5b77ee1
miro83kolev <miro83kolev@gmail.com>
2023-04-07 12:24:34 +0300

    update

---
+++ JS_Advanced/Exam preps/Furniture Store/app.js
@@ -0,0 +1,75 @@
+window.addEventListener('load', solve);
+
+function solve() {
+    let model = document.querySelector('#model');
+    let year = document.querySelector('#year');
+    let description = document.querySelector('#description');
+    let price = document.querySelector('#price');
+    let addBtn = document.querySelector('#add');
+
+    let furnitureList = document.querySelector('#furniture-list');
+    let totalPrice = document.querySelector('.total-price');
+
+    addBtn.addEventListener('click', validateAndAdd);
+
+    function validateAndAdd(e) {
+        if (model.value !== '' && year.value !== '' && description.value !== '' && price.value >= 0 
+        && Number(price.value) > 0 && Number(year.value) > 0) {
+            add(e, model.value, year.value, description.value, price.value);
+            clearInputs();
+        }
+    }
+
+    function add(e, model, year, description, price) {
+        e.preventDefault();
+
+        let firstTr = htmlGenerator('tr', '', furnitureList);
+        firstTr.setAttribute('class', 'info');
+        htmlGenerator('td', `${model}`, firstTr);
+        htmlGenerator('td', `${Number(price).toFixed(2)}`, firstTr);
+        let td = htmlGenerator('td', '', firstTr);
+
+        let moreBtn = htmlGenerator('button', 'More Info', td);
+        moreBtn.setAttribute('class', 'moreBtn');
+
+        let buyBtn = htmlGenerator('button', 'Buy it', td);
+        buyBtn.setAttribute('class', 'buyBtn');
+
+        let secondTr = htmlGenerator('tr', '', furnitureList);
+        secondTr.setAttribute('class', 'hide');
+        htmlGenerator('td', `Year: ${year}`, secondTr);
+        let descriptionTd = htmlGenerator('td', `Description: ${description}`, secondTr);
+        descriptionTd.setAttribute('colspan', 3);
+
+        moreBtn.addEventListener('click', (e) => {
+            e.target.textContent === 'More Info'
+                ? e.target.textContent = 'Less Info'
+                : e.target.textContent = 'More Info';
+            secondTr.style.display = 'none'
+                ? secondTr.style.display = 'contents'
+                : secondTr.style.display = 'none';
+        })
+
+        buyBtn.addEventListener('click', (e) => {
+            firstTr.remove();
+            secondTr.remove();
+            totalPrice.textContent = (Number(totalPrice.textContent) + Number(price)).toFixed(2);
+        })
+    }
+
+    function htmlGenerator(tagName, content, parent) {
+        let el = document.createElement(tagName);
+        el.textContent = content;
+        if (parent) {
+            parent.appendChild(el);
+        }
+        return el;
+    }
+
+    function clearInputs() {
+        model.value = '';
+        year.value = '';
+        description.value = '';
+        price.value = '';
+    }
+}
---
+++ JS_Advanced/Exam preps/Travel Agency/solution.js
@@ -0,0 +1,87 @@
+window.addEventListener('load', solution);
+
+function solution() {
+  // find all correct selectors
+  const fullName = document.querySelector("#fname");
+  const email = document.querySelector("#email");
+  const phone = document.querySelector("#phone");
+  const address = document.querySelector("#address");
+  const postalCode = document.querySelector("#code");
+
+  const submitBtn = document.querySelector("#submitBTN");
+  const editBtn = document.querySelector("#editBTN");
+  const continueBtn = document.querySelector("#continueBTN");
+
+  const infoPreview = document.querySelector("#infoPreview");
+  const divBlock = document.querySelector("#block");
+
+
+  submitBtn.addEventListener('click', (e) => {
+    // check if value 
+    if (!fullName.value || !email) {
+          return;
+      }
+      addInfo(e, fullName.value, email.value, phone.value, address.value, postalCode.value);
+      clearInputs();
+  });
+
+  // add info function
+  function addInfo(e, fname, mail, phoneNum, addresss, code) {
+      htmlGenerator('li', `Full Name: ${fname}`, infoPreview);
+      htmlGenerator('li', `Email: ${mail}`, infoPreview);
+      htmlGenerator('li', `Phone Number: ${phoneNum}`, infoPreview);
+      htmlGenerator('li', `Address: ${addresss}`, infoPreview);
+      htmlGenerator('li', `Postal Code: ${code}`, infoPreview);
+
+      submitBtn.setAttribute('disabled', 'disabled');
+      editBtn.removeAttribute('disabled');
+      continueBtn.removeAttribute('disabled');
+
+      editBtn.addEventListener('click', (e) => {
+          fullName.value = fname;
+          email.value = mail;
+          phone.value = phoneNum;
+          address.value = addresss;
+          postalCode.value = code;
+
+          submitBtn.removeAttribute('disabled');
+          editBtn.setAttribute('disabled', 'disabled');
+          continueBtn.setAttribute('disabled', 'disabled');
+
+          let allLis = infoPreview.querySelectorAll('li');
+          for (let li of allLis) {
+              li.remove();
+          }
+      })
+
+      continueBtn.addEventListener('click', (e) => { 
+          while (divBlock.firstChild) {
+              divBlock.removeChild(divBlock.lastChild);
+          }
+          htmlGenerator('h3', "Thank you for your reservation!", divBlock);
+      })
+
+
+  }
+
+  // html element creation function
+  function htmlGenerator(tagName, content, parent) {
+      let el = document.createElement(tagName);
+      el.textContent = content;
+      if (parent) {
+          parent.appendChild(el);
+      }
+      return el;
+  }
+
+  // clear inputs function
+  function clearInputs() {
+      fullName.value = '';
+      email.value = '';
+      phone.value = '';
+      address.value = '';
+      postalCode.value = '';
+  }
+
+
+}
